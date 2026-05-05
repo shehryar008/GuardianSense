344e576b594c00f4a83a589a32f1fceb176265a3
Abdullahar990 <abdullahar990@gmail.com>
2026-05-05 11:20:11 +0500

    Add Chatbot

--- Mobile Application/guardian_sense_1/lib/constants/app_constants.dart
+++ Mobile Application/guardian_sense_1/lib/constants/app_constants.dart
@@ -1,4 +1,6 @@
 class AppConstants {
   static const String supabaseUrl = 'https://imrxmhibxdtmggswgslv.supabase.co';
   static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltcnhtaGlieGR0bWdnc3dnc2x2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2NzM0NTIsImV4cCI6MjA4OTI0OTQ1Mn0.uz2oHlMX4d9GYdgaLPXMo0zcX-BZrQek0y4g_c0O3q4';
+  
+  static const String geminiApiKey = 'AIzaSyDbDEB3x2eVxniY5G_dmmLmrIQP9SMs5bU';
 }
